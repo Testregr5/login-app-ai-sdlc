e4ffffcd4e355c2fdcc7347017fe1dc3dae05158
Testregr5 <testregr5@gmail.com>
2026-04-18 18:36:53 +0530

    Update Pages.ts for EPMCDMETST-39332: ensure proper Pages holder implementation

--- pages/Pages.ts
+++ pages/Pages.ts
@@ -2,5 +2,10 @@ import { PageHolder } from './PageHolder';
 import { LoginAppPage } from './LoginAppPage';
 
 export class Pages extends PageHolder {
-  readonly loginAppPage: LoginAppPage = new LoginAppPage(this.page);
+  readonly loginAppPage: LoginAppPage;
+
+  constructor(page: any) {
+    super(page);
+    this.loginAppPage = new LoginAppPage(page);
+  }
 }
